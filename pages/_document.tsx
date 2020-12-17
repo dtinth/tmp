232566848798b0f26dbc43efdcf6fe93beee96b7
dtinth on Cloud9 <dtinth@spacet.me>
2020-12-17 05:58:09 +0000

    put footer to the right

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
         <body className="bg-#353433 text-white">
           <Main />
           <NextScript />
-          <footer className="mt-6 px-6 mb-2 text-xs text-#8b8685">
+          <footer className="mt-6 px-6 mb-2 text-xs text-#8b8685 text-right">
             built at {new Date().toJSON()} &middot;{' '}
             <a
               href="https://github.com/dtinth/tmp"
